e1f6dc1c4fb428767033980be452bc06a7ae1996
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-04-26 09:16:18 +0300

    fix(App.tsx): add state for initial values

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 
-function App() {
+const App = () => {
+  const [apiKey, setApiKey] = useState<string>('');
+  const [open, setOpen] = useState<boolean>(false);
+  const [properties, setProperties] = useState<[]>([]);
+
+  useEffect(() => {
+    if (apiKey === '') setOpen(true);
+  }, [apiKey]);
+
   return (
     <div className={'bodyContainer'}>
       <div className={'bodyInner'}>
         <Routes>
           <Route path={'/'} element={<></>} />
         </Routes>
+        {apiKey === '' && properties.length === 0 && (
+          <p>
+            Oops.
+            <br /> Looks like you need to enter your key.
+          </p>
+        )}
+        {apiKey !== '' && properties.length === 0 && (
+          <p>There are no listings to display</p>
+        )}
       </div>
     </div>
   );
-}
+};
 
 export default App;
