e02b27df03d5e36e0aa4948b2bf6573834206045
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-04-26 09:15:41 +0300

    fix(.json): add rules to placate eslint & TS errors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,8 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh', '@typescript-eslint', 'prettier'],
   rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     'prettier/prettier': 'error',
   },
