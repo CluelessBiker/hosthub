091510079a276eda5f719f643368f05da04ae10d
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-04-27 14:01:11 +0300

    fix(RentalData.tsx): display additional data with boolean prop

--- src/components/RentalData.tsx
+++ src/components/RentalData.tsx
@@ -1,11 +1,23 @@
 import { FC } from 'react';
 import { Rental } from '../types/Rental';
+import { useNavigate } from 'react-router-dom';
+import btn from '../styles/Buttons.module.css';
+import { useSettings } from '../context/SettingsContext';
+import formatTime from '../utils/formatTime';
 
 type Props = {
   data: Rental;
+  fullDetails?: boolean;
 };
 
-const RentalData: FC<Props> = ({ data }) => {
+const RentalData: FC<Props> = ({ data, fullDetails = false }) => {
+  const navigate = useNavigate();
+  const settings = useSettings();
+
+  const handleGoHome = () => {
+    navigate('/');
+  };
+
   return (
     <div>
       <p>{data.city}</p>
@@ -18,9 +30,29 @@ const RentalData: FC<Props> = ({ data }) => {
         />
       )}
 
-      <a href={`/property/${data.id}`} aria-label={`view property details`}>
-        view more
-      </a>
+      {!fullDetails && (
+        <a href={`/rental/${data.id}`} aria-label={`view property details`}>
+          view more
+        </a>
+      )}
+
+      {fullDetails && (
+        <>
+          <p>{formatTime(data.check_in_time, settings.timeFormat)}</p>
+          <p>{formatTime(data.checkout_time, settings.timeFormat)}</p>
+          <p>
+            {data.city}, {data.postal_code}, {data.country}
+          </p>
+          <p>Property manager: {data.checkout_time}</p>
+          <a
+            className={btn.btn}
+            onClick={handleGoHome}
+            aria-label={'return to home page'}
+          >
+            back
+          </a>
+        </>
+      )}
     </div>
   );
 };
