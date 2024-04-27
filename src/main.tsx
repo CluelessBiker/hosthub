a40ac7318cf75580d40f363858541c4275a1363e
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-04-27 13:00:54 +0300

    fix(main.tsx): add SettingsContext wrapper

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './i18n';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { SettingsContextProvider } from './context/SettingsContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <SettingsContextProvider>
+        <App />
+      </SettingsContextProvider>
     </Router>
   </React.StrictMode>,
 );
