173e3541d50c6a6fac9e418b7f8306c7a7c626a9
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-04-27 12:17:57 +0300

    feat(Rental.ts): create typeset

---
+++ src/types/Rental.ts
@@ -0,0 +1,18 @@
+export type Rental = {
+  check_in_time: string;
+  checkout_time: string;
+  city: string;
+  country: string;
+  currency: string;
+  id: string;
+  image_path: string;
+  latitude: number;
+  longitude: number;
+  name: string;
+  object: string;
+  postal_code: string;
+  rates_confirmed: boolean;
+  status: string;
+  time_zone: string;
+  url: string;
+};
