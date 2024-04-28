b54930a65aacce3e3b7218c3692b877e75fa8444
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-04-28 20:54:36 +0300

    fix(formatDate.ts): remove T split

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,9 +1,8 @@
 const formatDate = (dateTime: string, format: string, separator: string) => {
-  const date = dateTime.split('T')[0];
-  const newDate = date.split('-');
-  const year = newDate[0];
-  const month = newDate[1];
-  const day = newDate[2];
+  const date = dateTime.split('-');
+  const year = date[0];
+  const month = date[1];
+  const day = date[2];
 
   if (format === 'us') return month + separator + day + separator + year;
   return day + separator + month + separator + year;
