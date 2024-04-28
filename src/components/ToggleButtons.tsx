cb32d94216b2e75e841a591f4e4ebfd3108d28db
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-04-28 17:29:53 +0300

    fix(ToggleButtons.tsx): remove interface & import typeset

--- src/components/ToggleButtons.tsx
+++ src/components/ToggleButtons.tsx
@@ -1,18 +1,13 @@
 import { FC, MouseEvent } from 'react';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
-
-interface Options {
-  value: string;
-  ariaLabel: string;
-  text: string;
-}
+import { ToggleOptions } from '../types/ToggleOptions';
 
 type Props = {
   id: string;
   label: string;
   value: string;
-  data: Options[];
+  data: ToggleOptions[];
   onChange: (event: MouseEvent<HTMLElement>, value: string) => void;
 };
 
@@ -30,7 +25,7 @@ const ToggleButtons: FC<Props> = ({ id, label, value, data, onChange }) => {
         aria-label={id}
         onChange={onChange}
       >
-        {data.map((it: Options, index) => (
+        {data.map((it: ToggleOptions, index) => (
           <ToggleButton key={index + it.value} value={it.value} aria-label={it.ariaLabel}>
             <p>{it.text}</p>
           </ToggleButton>
