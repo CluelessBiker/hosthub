6cedc673dfb1a6e03625fbde27827e03f9eeb413
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-04-28 17:14:07 +0300

    feat(constants.ts): move static data into own file

---
+++ src/constants/constants.ts
@@ -0,0 +1,27 @@
+const timeSelections = [
+  {
+    value: '24',
+    ariaLabel: '24 hour time format',
+    text: '24H',
+  },
+  {
+    value: '12',
+    ariaLabel: '12 hour time format',
+    text: '12H',
+  },
+];
+
+const dateSelections = [
+  {
+    value: 'eu',
+    ariaLabel: 'EU date format',
+    text: 'DD-MM-YYYY',
+  },
+  {
+    value: 'us',
+    ariaLabel: 'US date format',
+    text: 'MM-DD-YYYY',
+  },
+];
+
+export { timeSelections, dateSelections };
